c3b5a80adb6ea2a69c8bb1cc416b5e362704d123
faseekhan575 <su92-bscsm-f23-275@superior.edu.pk>
2025-08-12 16:48:57 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,28 +3,23 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
-  : [];
+const allowedOrigins = process.env.ALLOWED_ORIGIN
+? process.env.ALLOWED_ORIGIN.split(',').map(origin => origin.trim())
+: [];
 
+  app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+}));
 app.use(express.json());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow non-browser tools
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-}));
-
-app.options('*', cors()); // enable preflight requests for all routes
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -33,13 +28,6 @@ app.get('/', (req, res) => {
 const routes = require('./routes/mail');
 app.use("/api/mail", routes);
 
-app.use((err, req, res, next) => {
-  if (err.message === 'Not allowed by CORS') {
-    return res.status(403).send('CORS Error: Origin not allowed');
-  }
-  next(err);
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+}); 
