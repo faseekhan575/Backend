a5654ebe0fae723f6900415786f276ed258c2925
faseekhan575 <su92-bscsm-f23-275@superior.edu.pk>
2025-08-12 17:17:42 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,21 +5,8 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
+app.use(cors())
 app.use(express.json());
-const allowedOrigins = process.env.ALLOWED_ORIGIN
-  ? process.env.ALLOWED_ORIGIN.split(',').map(origin => origin.trim())
-  : [];
-console.log(allowedOrigins);
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
-}));
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
